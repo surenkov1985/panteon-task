35f13e49305ffaf5802ab6bbcdec0d803f561eb3
mich-man <88162731+surenkov1985@users.noreply.github.com>
2023-01-04 15:55:07 +0300

    добавление бутстрапа, изменение верстки header с помощью классов бутстрап

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import * as bootstrap from "bootstrap";
+import $ from "jquery";
+
+// styles
+
 import "./styles/style.scss";
 
+// javascript
+
+import "./js/yandex-map";
+
 console.log("hello!");
+
+$(".toggler-icon").on("click", function () {
+	$(this).toggleClass("active");
+});
