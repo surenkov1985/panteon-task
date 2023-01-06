44b862d1417a32f214cc8c4955504c3081342ef5
mich-man <88162731+surenkov1985@users.noreply.github.com>
2023-01-06 22:00:27 +0300

    добавлены js анимации, срколлы, модальное окно, декор

---
+++ src/js/scroll-listeners.js
@@ -0,0 +1,102 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+	let animated = false;
+	let windowHeight = $(window).innerHeight();
+	let documentPosition = $(document).scrollTop();
+
+	if ($(document).scrollTop() < windowHeight / 2) {
+		$("html, body").animate(
+			{
+				scrollTop: 0,
+			},
+			100,
+			function () {
+				$(document).scrollTop(0);
+				documentPosition = 0;
+			}
+		);
+	}
+
+	if ($(document).scrollTop() < windowHeight && $(document).scrollTop() > windowHeight / 2) {
+		$("html, body").animate(
+			{
+				scrollTop: windowHeight,
+			},
+			100,
+			function () {
+				$(document).scrollTop(windowHeight);
+				documentPosition = windowHeight;
+			}
+		);
+	}
+
+	$(document).on("scroll", function (e) {
+		if (animated) return;
+		let scrollTop = $(document).scrollTop();
+
+		if (documentPosition === 0 && scrollTop > documentPosition) {
+			animated = true;
+			$("html, body").animate(
+				{
+					scrollTop: windowHeight,
+				},
+				300,
+				function () {
+					documentPosition = windowHeight;
+					animated = false;
+				}
+			);
+		}
+
+		if (documentPosition === windowHeight && scrollTop < documentPosition) {
+			animated = true;
+			$("html, body").animate(
+				{
+					scrollTop: 0,
+				},
+				300,
+				function () {
+					documentPosition = 0;
+					animated = false;
+				}
+			);
+		}
+		$("html, body").animate({});
+	});
+
+	$(".head__scroll-btn").on("click", function (e) {
+		animated = true;
+		$("html, body").animate(
+			{
+				scrollTop: windowHeight,
+			},
+			300,
+			function () {
+				documentPosition = windowHeight;
+				$(document).scrollTop(windowHeight);
+				animated = false;
+			}
+		);
+	});
+
+	$(".link").each(function (_, link) {
+		$(link).on("click", function (e) {
+			e.preventDefault();
+			animated = true;
+
+			let sectionPosition = $(e.target.hash).offset().top;
+
+			$("html, body").animate(
+				{
+					scrollTop: sectionPosition,
+				},
+				300,
+				function () {
+					documentPosition = sectionPosition;
+					animated = false;
+				}
+			);
+		});
+	});
+});
